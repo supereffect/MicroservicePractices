73ff24dbd498ee0bfd95b20095c7dd39df1badcd
Egehan Çakır <e.cakir@baylan.local>
2024-07-03 17:45:13 +0300

    Platform Controller Aded Get Method Added, Mapper Refactor

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.DTOs;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]}")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            var platformItem = _repository.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+
+    }
+}

86ad9fed9289971252f88b4fb0ea890d767520b4
Egehan Çakır <e.cakir@baylan.local>
2024-07-04 12:08:27 +0300

    Get By Id & Create added

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -2,10 +2,11 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.DTOs;
+using PlatformService.Models;
 
 namespace PlatformService.Controllers
 {
-    [Route("api/[controller]}")]
+    [Route("api/[controller]")]
     [ApiController]
     public class PlatformsController : ControllerBase
     {
@@ -23,6 +24,29 @@ namespace PlatformService.Controllers
         {
             var platformItem = _repository.GetAllPlatforms();
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+            if (platformItem != null)
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+
+            var platformModel = _mapper.Map<Platform>(platformCreateDto);
+            _repository.CreatePlatform(platformModel);  
+            _repository.SaveChanges();  
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);    
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformReadDto.Id},platformReadDto);
+
         }
 
     }
